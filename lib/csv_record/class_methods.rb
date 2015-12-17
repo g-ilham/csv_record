9355a8f79dcc700266e8f69da41d17f6b0c5125d
Ilgam Gaysin <ilgamgaysin@gmail.com>
2015-12-17 17:44:19 +0300

    [Csv readers | all] added base class

--- lib/csv_record/class_methods.rb
+++ lib/csv_record/class_methods.rb
@@ -3,5 +3,9 @@ module CsvRecord
     def create(attrs)
       CsvRecord::Persistance::Create.new(self, attrs).run
     end
+
+    def all
+      CsvRecord::Readers::Base.all(self)
+    end
   end
 end
---
+++ lib/csv_record/readers/base.rb
@@ -0,0 +1,10 @@
+module CsvRecord
+  module Readers
+    class Base
+      class << self
+        def all(target_class)
+        end
+      end
+    end
+  end
+end
