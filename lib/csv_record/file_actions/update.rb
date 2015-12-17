d11d9ea8edda8077abc8d0ff21f1672d98b48180
Ilgam Gaysin <ilgamgaysin@gmail.com>
2015-12-17 16:55:15 +0300

    [Csv manager | updater] added update found record with validation

--- lib/csv_record/file_actions/update.rb
+++ lib/csv_record/file_actions/update.rb
@@ -4,6 +4,22 @@ module CsvRecord
   module FileActions
     class Update < CsvRecord::FileActions::Base
       def update
+        set_existing_csv
+        update_record!
+      end
+
+      private
+
+      def update_record!
+        find_record
+
+        attrs.each do |key, value|
+          if csv_headers.include?(key.to_sym)
+            self.found_record[key.to_sym] = value
+          else
+            raise "#{key} attribute is not present in table"
+          end
+        end
       end
     end
   end
