4abe970a70774f8dd008c3dc141d6db9d23b3d39
Ilgam Gaysin <ilgamgaysin@gmail.com>
2015-12-17 17:58:43 +0300

    [Csv readers | all] done conf base classes

---
+++ lib/csv_record/file_actions/finder.rb
@@ -0,0 +1,10 @@
+require "#{File.dirname(__FILE__) + '/base.rb'}"
+
+module CsvRecord
+  module FileActions
+    class Finder < CsvRecord::FileActions::Base
+      def all
+      end
+    end
+  end
+end
--- lib/csv_record/readers/base.rb
+++ lib/csv_record/readers/base.rb
@@ -3,6 +3,7 @@ module CsvRecord
     class Base
       class << self
         def all(target_class)
+          CsvRecord::FileActions::Finder.new(target_class).all
         end
       end
     end
