43fd7edebc3d398f5a164f89b56443470646325d
Ilgam Gaysin <ilgamgaysin@gmail.com>
2015-12-18 15:50:10 +0300

    [Unit tests] added for last and destroy actions

--- test/csv_record_test.rb
+++ test/csv_record_test.rb
@@ -47,6 +47,20 @@ class CsvRecordTest < ActiveSupport::TestCase
     assert_equal updated_name, updated_record.first_name
   end
 
+  test "destroy" do
+    clean_csv_file!
+    create_customer!(first_name, last_name)
+
+    created_record = csv_entries.first
+    found_record = Customer.find(created_record[:id])
+
+    found_record.destroy
+
+    assert_raise RuntimeError do
+      Customer.find(created_record[:id])
+    end
+  end
+
   test "all" do
     clean_csv_file!
     create_collection!
@@ -66,6 +80,16 @@ class CsvRecordTest < ActiveSupport::TestCase
 
     assert_equal first_name, first_customer.first_name
     assert_equal last_name, first_customer.last_name
+  end
+
+  test "last" do
+    clean_csv_file!
+    create_collection!
+
+    last_customer = Customer.last
+
+    assert_equal "Ilgis", last_customer.first_name
+    assert_equal "Ibragimov", last_customer.last_name
   end
 
   def first_name
