f79e27761b11a562b00966e649c2a322610eff82
Ilgam Gaysin <ilgamgaysin@gmail.com>
2015-12-18 10:40:07 +0300

    [Csv readers | all] done all scope

--- lib/csv_record/file_actions/finder.rb
+++ lib/csv_record/file_actions/finder.rb
@@ -4,6 +4,11 @@ module CsvRecord
   module FileActions
     class Finder < CsvRecord::FileActions::Base
       def all
+        set_existing_csv
+
+        existing_file.map do |csv_record|
+          target_class.new(csv_record)
+        end
       end
     end
   end
